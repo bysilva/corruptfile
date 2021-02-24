d3b9b701c9ad65ea68ce1f90e665923bb48bbefd
Edgar eduardo <eepsmr48@hotmail.com>
2021-02-23 22:27:02 -0600

    Revert "Change"
    This reverts commit bb364c16852539542536049defe313125f251342.

--- corruptfile/MainWindow.xaml.cs
+++ corruptfile/MainWindow.xaml.cs
@@ -54,7 +54,7 @@ namespace corruptfile
                 winword.Quit(ref missing, ref missing, ref missing);
                 winword = null;
                 CorrupFile(filename.ToString());
-                MessageBox.Show($"Archivo creado correctamente {filename}");
+                MessageBox.Show($"Archivo creado correctamente {filename.ToString()}");
             }
             catch (Exception ex)
             {
